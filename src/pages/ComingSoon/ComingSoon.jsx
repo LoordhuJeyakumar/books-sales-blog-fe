1029601e7423b1288e0ecbb3b3afaae2a82d8c77
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2024-11-25 15:18:12 +0530

    feat(ComingSoon): implement coming soon page with basic layout and styling

---
+++ src/pages/ComingSoon/ComingSoon.jsx
@@ -0,0 +1,12 @@
+import styles from "./ComingSoon.module.css";
+const ComingSoon = () => {
+  return (
+    <main className={styles.container}>
+      <div>
+        <h1 className={styles.text}>Coming soon !</h1>
+      </div>
+    </main>
+  );
+};
+
+export default ComingSoon;
