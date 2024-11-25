3eb802fa3a135157c568c91cbe49ee185bdc6549
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2024-11-25 15:18:26 +0530

    feat(NotFound): implement not found page with basic layout and styling

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,22 @@
+import styles from "./NotFound.module.css";
+import { Link } from "react-router-dom";
+import notfound from "../../images/404.png";
+const NotFound = () => {
+  return (
+    <main className={styles.container}>
+      <div className={styles.pageError}>Page Error</div>
+      <div className={styles.mainContent}>
+        <img src={notfound} alt="404" />
+        <p>Page not Found!!!</p>
+        <span>
+          The page you are looking for doesn&apos;t exist. Please try searching
+          for some other page, or return to the website&apos;s homepage to find
+          what you&apos;re looking for.
+        </span>
+        <Link to="/">Back to Home</Link>
+      </div>
+    </main>
+  );
+};
+
+export default NotFound;
